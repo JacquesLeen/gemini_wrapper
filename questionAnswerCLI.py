8a3c1ae4a6bd1dd861dee643ddce474dfc4053aa
JacquesLeen <56220139+JacquesLeen@users.noreply.github.com>
2025-11-26 13:22:33 +0000

    fixed cli command

--- questionAnswerCLI.py
+++ questionAnswerCLI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-CLI to to submit question and interact with Gemini 2.5 via Vertex AI API.
+CLI to to submit question and interact with Gemini 2.5
 """
 
 import os
@@ -13,6 +13,7 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_SECRET"))
 
 # define main function to call the bot
 def question(text: str):
+    """Function to send a question to Gemini 2.5 and get a response."""
     response = client.models.generate_content(model="gemini-2.5-flash", contents=text)
     return response.text
 
@@ -21,19 +22,13 @@ def question(text: str):
 @click.command()
 @click.option("--text", required=True, help="Text to generate content for.")
 def cli(text):
+    """CLI to interact with Gemini 2.5."""
     result = question(text)
     print("Generated Content:")
     print(result)
 
 
 if __name__ == "__main__":
-    cli()
+    cli()  # pylint: disable=no-value-for-parameter
 
 
-# while True:
-#     message = input("> ")
-#     if message == "exit":
-#         break
-
-#     result = chat.send_message(message)
-#     print(result.text)
